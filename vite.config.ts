dcbdcdacea1f90920585a99bc5d675e2b18bfe82
mukeshyadavp <pmukeshyadav12@gmail.com>
2026-03-31 17:28:32 +0530

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -2,27 +2,5 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-
-  resolve: {
-    dedupe: ["react", "react-dom"],
-    alias: {
-      react: "react",
-      "react-dom": "react-dom"
-    }
-  },
-
-  optimizeDeps: {
-    include: ["react", "react-dom", "react/jsx-runtime"]
-  },
-
-  build: {
-    rollupOptions: {
-      external: [], // force bundling
-    },
-    commonjsOptions: {
-      include: [/node_modules/],
-      transformMixedEsModules: true
-    }
-  }
+  plugins: [react()]
 });
